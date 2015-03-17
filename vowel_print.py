d6949ccdb9eebb9da0dee16214d115957b8fd1a4
jstocke2 <jake.stocker@yahoo.com>
2015-03-16 21:05:56 -0400

    Looks for aeiou in sequential order in a large block of text

---
+++ vowel_print.py
@@ -0,0 +1,58 @@
+###################################################################
+#Jake Stocker                                                     #
+#07/11/2014                                                       #
+#                                                                 #
+#This program counts how many words from file words.txt           #
+#contain the vowels a e i o u in sequential order                 #
+#                                                                 #
+#The function has been written to allow generalization of the     #
+#strings compared.  To change the strings compared change the     #
+#the values of the strings passed in to contains_letters_in_order #
+###################################################################
+
+
+###IMPORTANT###You will need to modify the directory of the file for your hardrive
+
+
+fin=open("/home/jake/Desktop/words.txt")
+LINE=fin.readline
+VOWELS="aeiou"
+
+#This function returns true if every letter of string lettersCompared can be matched with every
+#letter in the string word in order
+
+
+def contains_letters_in_order(word, lettersCompared):
+    matches = 0
+    for letter in word:
+        if letter == lettersCompared[matches]:
+            matches += 1
+            if matches == len(lettersCompared):
+                return True
+    return False
+
+
+#Main() reads all lines in the file and then tests each string passed by contains_vowels_in_order()
+#if the string contains all letters of string lettersCompared in order the word is printed.  
+
+
+def main():
+    count=0
+    for LINE in fin:
+        if contains_letters_in_order(LINE,VOWELS)==True:
+            print LINE
+            
+                   
+
+
+
+main()
+
+          
+      
+    
+    
+    
+
+
+
